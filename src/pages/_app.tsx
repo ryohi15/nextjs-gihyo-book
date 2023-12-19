9d4d1edef2aa02d4ba3bbebddbc749d95c101a99
ryo-h <ryo-h@weseek.co.jp>
2023-12-19 21:00:04 +0900

    5.6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { createGlobalStyle } from "styled-components";
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 html,
@@ -26,7 +26,7 @@ a {
 ol, ul {
   list-style: none;
 }
-`;
+`
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -44,7 +44,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <GlobalStyle />
       <Component {...pageProps} />
     </>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
